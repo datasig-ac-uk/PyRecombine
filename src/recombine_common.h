0dd7ef1c5533d132dd95612b185d18082feb74a4
Sam Morley <admin@inakleinbottle.com>
2024-10-19 22:56:05 +0100

    Reorganize typedef declarations in recombine_common.h
    Move `index_integer` typedef after `doublereal` typedef for consistency. This change ensures all type definitions are grouped logically, improving code readability.

--- src/recombine_common.h
+++ src/recombine_common.h
@@ -15,19 +15,17 @@
 #if INTPTR_MAX == INT64_MAX
 #if RECOMBINE_INT_SIZE == 8
 typedef ptrdiff_t integer;
-typedef size_t index_integer;
 #elif RECOMBINE_INT_SIZE == 4
 typedef int32_t integer;
-typedef uint32_t index_integer;
 #else
 #error "Valid choices for integer are 4 bytes or 8 bytes"
 #endif
 #else
 typedef ptrdiff_t integer;
-typedef size_t index_integer;
 #endif
 
 typedef double doublereal;
+typedef size_t index_integer;
 
 
 
